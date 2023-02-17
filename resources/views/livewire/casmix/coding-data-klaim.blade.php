cb3ac9573b21e320d6ad3949137f3e6d622bc036
virusphp <virusp_php@yahoo.com>
2023-02-17 12:01:11 +0700

    fixed tab jenis rawat radio hak kelas

--- resources/views/livewire/casmix/coding-data-klaim.blade.php
+++ resources/views/livewire/casmix/coding-data-klaim.blade.php
@@ -86,13 +86,13 @@
 					<td class="m-1">
 						<input id="kelas-hak-2" type="radio" value="2" name="kelas_hak"
 							class="ml-4 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-1 dark:bg-gray-700 dark:border-gray-600">
-						<label for="kelas-hak-3"
+						<label for="kelas-hak-2"
 							class="text-sm font-medium text-gray-900 dark:text-gray-300 underline">Kelas 1</label>
 					</td>
 					<td class="m-1">
-						<input id="kelas-hak-2" type="radio" value="1" name="kelas_hak"
+						<input id="kelas-hak-1" type="radio" value="1" name="kelas_hak"
 							class="ml-4 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-1 dark:bg-gray-700 dark:border-gray-600">
-						<label for="kelas-hak-3"
+						<label for="kelas-hak-1"
 							class="text-sm font-medium text-gray-900 dark:text-gray-300 underline">Kelas 1</label>
 					</td>
 				</div>
